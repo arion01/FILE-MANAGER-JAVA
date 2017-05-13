a8b66695549beccdd8cc5de1d638396740722f2b
arion01 <arionproger@gmail.com>
2017-05-13 18:59:39 +0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+import Utils.IFILE;
+import Utils.Files;
+
+public class Main {
+	
+	static IFILE file = new Files("storage/emulated/0/AppProjects/JavaApp/");
+	
+	static Scanner scn = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+	}
+}
